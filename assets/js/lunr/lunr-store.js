98bb759df664ee85d21f4e46d953f8a5cf7ac54d
kyngon <kyngon@users.noreply.github.com>
2021-02-07 20:05:45 +0000

    jekyll build from Action b7dc28f679c079f53081024a118bb21212515ade

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Blog Introduction",
+        "excerpt":"Welcome to my blog about genealogy! The idea for this blog came to me at the end of 2020, as a way to document what I discovered during that year about genealogy, family trees, looking for information about other family members online, compiling a database with all these information, and...","categories": ["blog"],
+        "tags": ["blog","introduction"],
+        "url": "http://familytreehowto.com/blog/blog-introduction/",
+        "teaser": null
+      },{
+        "title": "What is a family tree and why should I bother",
+        "excerpt":"Have you ever been lost in a big city, all streets looking alike and trying to find your way out of the maze and back to known parts? A family is similar to a city in some aspects. You know all about the small parts of the family that is...","categories": ["blog"],
+        "tags": ["blog","genealogy","family-tree"],
+        "url": "http://familytreehowto.com/blog/why-genealogy/",
+        "teaser": null
+      }]
